30e04a6f275639d793ab3ffe706e18268094b025
Louis Schnellbach <xleek.okz@gmail.com>
2012-02-03 15:29:44 +0100

    Directly write after reading rather than storing everything into a vector

--- TriFusion/main.cpp
+++ TriFusion/main.cpp
@@ -3,38 +3,30 @@
 #include <fstream>
 #include <vector>
 
-void createBinaryFile(const std::string& input) {
-	//Open file
-	std::string output = input + "_random.bin";
+void createBinaryFile(const std::string& input)
+{
+    //Open file
+    std::string output = input + "_random.bin";
 
-	std::ofstream ofile(output.c_str(), std::ios::binary);
-	std::ifstream ifile(input.c_str(), std::ios::binary);
+    std::ofstream ofile(output.c_str(), std::ios::binary);
+    std::ifstream ifile(input.c_str(), std::ios::binary);
 
-	std::vector<int> value;
-	//Read and put each number into value vector
-	//If we can't read the file, we won't convert anything
-	if(ifile) {
-		int tmp;	
-		while(!ifile.eof()) {
-			ifile >> tmp;
-			value.push_back(tmp);
-		}
-		ifile.close();
+    //Read and put each number into value vector
+    //If we can't read the file, we won't convert anything
+    if(ifile) {
+        int tmp = 0;
+        while(!ifile.eof()) {
+            ifile >> tmp;
+            if(ofile) {
+                //Use a space to not have a HUGE number
+                //(but rather a number list)
+                ofile.write((char*)&tmp, sizeof(int));
+            }
+        }
+        ofile.close();
+        ifile.close();
 
-	//Now we have our value, write them
-	if(ofile) {
-		//We are going to read data back to front
-		//So, reverse our vector
-		std::reverse(value.begin(), value.end());
-		while(!value.empty()) {
-			//Use a space to not have a HUGE number
-			//(but rather a number list)
-			ofile.write((char*)&value.back(), sizeof(int));
-			value.pop_back();
-		}
-		ofile.close();
-	}
-	}
+    }
 }
 
 int main(int argc, char** argv) 
