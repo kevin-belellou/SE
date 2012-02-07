a08608aaf8e6356d43cdd1a9eb7c14962ca0ce8e
Louis Schnellbach <xleek.okz@gmail.com>
2012-02-07 17:12:57 +0100

    Still doesn't work

--- TriFusion/main.cpp
+++ TriFusion/main.cpp
@@ -1,4 +1,3 @@
-#include <algorithm>
 #include <iostream>
 #include <fstream>
 #include <vector>
@@ -10,95 +9,108 @@
 
 int callFork(int min, int max);
 
+std::ifstream readedFile;
 void createBinaryFile(const std::string& input)
 {
-    //Open file
-    std::string output = input + "_random.bin";
+     //Open file
+     std::string output = input + "_random.bin";
 
-    std::ofstream ofile(output.c_str(), std::ios::binary);
-    std::ifstream ifile(input.c_str(), std::ios::binary);
+     std::ofstream ofile(output.c_str(), std::ios::binary);
+     std::ifstream ifile(input.c_str(), std::ios::binary);
 
-    //Read and put each number into value vector
-    //If we can't read the file, we won't convert anything
-    if(ifile) {
-        int tmp = 0;
-        ifile >> tmp;
-        while(!ifile.eof()) {
-            //Use a space to not have a HUGE number
-            //(but rather a number list)
-            ofile.write((char*)&tmp, sizeof(int));
-            //Read next value
-            ifile >> tmp;
-        }
-        ofile.close();
-        ifile.close();
+     //Read and put each number into value vector
+     //If we can't read the file, we won't convert anything
+     if(ifile) {
+          int tmp = 0;
+          ifile >> tmp;
+          while(!ifile.eof()) {
+               //Use a space to not have a HUGE number
+               //(but rather a number list)
+               ofile.write((char*)&tmp, sizeof(int));
+               //Read next value
+               ifile >> tmp;
+          }
+          ofile.close();
+          ifile.close();
 
-    }
+     }
 }
 
 
 int main(int argc, char** argv)
 {
-    std::string input;
-    std::string output;
-    //Check argument
-    if(argc == 2) {
-        input = output = argv[1];
-    } else {
-        std::cerr << "Wrong number of argument" << std::endl;
-        return -1;
-    }
-    //Convert the plain text
-    createBinaryFile(argv[1]);
+     std::string input;
+     std::string output;
+     //Check argument
+     if(argc == 2) {
+          input = output = argv[1];
+     } else {
+          std::cerr << "Wrong number of argument" << std::endl;
+          return -1;
+     }
+     //Convert the plain text
+     createBinaryFile(argv[1]);
 
-    //Create filename
-    output += "_sorted.bin";
-    input += "_random.bin";
+     //Create filename
+     output += "_sorted.bin";
+     input += "_random.bin";
 
-    //Open file
-    std::ofstream ofile(output.c_str(), std::ios::binary);
-    std::ifstream ifile(input.c_str(), std::ios::binary);
+     //Open file
+     std::ofstream ofile(output.c_str(), std::ios::binary);
+     std::ifstream ifile(input.c_str(), std::ios::binary);
 
 
-    unsigned int min = 0, max = 0;
-    //Read
-    if(ifile) {
-        //Go to the end, tell us the size
-        ifile.seekg(0, std::ios_base::end);
-        max = ifile.tellg();
-        //And return at the beginning of the file
-        ifile.seekg(0, std::ios::beg);
-        int tmp;
-        while(ifile.read((char*)&tmp, sizeof(int))) {
-            //Do some stuff with data
-        }
-    }
+     unsigned int min = 0, max = 0;
+     //Read
+     if(ifile) {
+          //Go to the end, tell us the size
+          ifile.seekg(0, std::ios_base::end);
+          max = ifile.tellg();
+          //And return at the beginning of the file
+          ifile.seekg(0, std::ios::beg);
+          int tmp;
+          std::cout << "Value are: " << std::endl;
+          while(ifile.read((char*)&tmp, sizeof(int))) {
+               std::cout << tmp << std::endl;
+          }
+     }
 
-	//First part of the merging sort
-	callFork(min, max);
-    
-    ofile.close();
-    ifile.close();
-    return 0;
+     //First part of the merging sort
+     readedFile.open(input.c_str());
+     callFork(min, max);
+     readedFile.close();
+
+     ofile.close();
+     ifile.close();
+     return 0;
 }
 
 int callFork(int min, int max) {
-    pid_t son = fork();
-    if(son == -1) {
-        //Seems something went wrong
-        std::cerr << "Fork failed" << std::endl;
-        return(-1);
-    } else if(son == 0) {
-        //Son's code
-        std::cout << "Fork success for value: " << min << "/" << max << " (" << getpid() << ")" << std::endl;
-		if(min >= max) {
-		} else {
-			callFork(min, max/2);
-		}
-        return 0;
-    } else {
-        wait(NULL);
-        std::cout << "Son fork ended" << son << std::endl;
-    }
-	return 0;
+     pid_t son = fork();
+	switch(son) {
+	case 0:
+          std::cout << "Fork success for value: " << min << "/" << max << " (" << getpid() << ")" << std::endl;
+          if(min >= max) {
+               int val = 0;
+               readedFile.seekg(max * sizeof(int));
+               readedFile.read((char*)&val, sizeof(int));
+               std::cout << "So value is: " << val << std::endl;
+               return val;
+          } else {
+               callFork(min, max/2);
+          	wait(NULL);
+          }
+          return 0;
+     default:
+          callFork(max/2+1, max);
+          wait(NULL);
+          std::cout << "Son fork ended : " << son << std::endl;
+     }
+     return 0;
+}
+
+int doSonFork(int min, int max)
+{
+     //Son's code
+
 }
