e816d8033f90fa99a9233986073bbbaeff8caec6
Belellou Kevin <gund38@gmail.com>
2012-02-15 11:16:11 +0100

    Comment's update

--- TriFusion/main.cpp
+++ TriFusion/main.cpp
@@ -11,22 +11,22 @@ int callFork(unsigned int min, unsigned int max);
 
 void createBinaryFile(const std::string& input)
 {
-     //Open file
+     // Open file
      std::string output = input + "_random.bin";
 
      std::ofstream ofile(output.c_str(), std::ios::binary);
      std::ifstream ifile(input.c_str(), std::ios::binary);
 
-     //Read and put each number into value vector
-     //If we can't read the file, we won't convert anything
+     // Read and put each number into value vector
+     // If we can't read the file, we won't convert anything
      if(ifile) {
           int tmp = 0;
           ifile >> tmp;
           while(!ifile.eof()) {
-               //Use a space to not have a HUGE number
-               //(but rather a number list)
+               // Use a space to not have a HUGE number
+               // (but rather a number list)
                ofile.write((char*)&tmp, sizeof(int));
-               //Read next value
+               // Read next value
                ifile >> tmp;
           }
           ofile.close();
@@ -37,9 +37,9 @@ void createBinaryFile(const std::string& input)
 
 void createAnsiFile(const std::string& input)
 {
-     //Open file
+     // Open file
      std::string output = input;
-	output.resize(output.size()-4); //remove the .bin
+	output.resize(output.size()-4); // remove the .bin
 
      std::ofstream ofile(output.c_str(), std::ios::binary);
      std::ifstream ifile(input.c_str(), std::ios::binary);
@@ -60,32 +60,32 @@ int main(int argc, char** argv)
 {
      std::string input;
      std::string output;
-     //Check argument
+     // Check argument
      if(argc == 2) {
           input = output = argv[1];
      } else {
           std::cerr << "Wrong number of argument" << std::endl;
           return -1;
      }
-     //Convert the plain text
+     // Convert the plain text
      createBinaryFile(argv[1]);
 
-     //Create filename
+     // Create filename
      output += "_sorted.bin";
      input += "_random.bin";
 
-     //Open file
+     // Open file
      std::ofstream ofile(output.c_str(), std::ios::binary);
      std::ifstream ifile(input.c_str(), std::ios::binary);
 
 
      unsigned int min = 0, max = 0;
-     //Read
+     // Read
      if(ifile) {
-          //Go to the end, tell us the size
+          // Go to the end, tell us the size
           ifile.seekg(0, std::ios_base::end);
           max = ifile.tellg();
-          //And return at the beginning of the file
+          // And return at the beginning of the file
           ifile.seekg(0, std::ios::beg);
           int tmp;
           std::cout << "Values are: " << std::endl;
@@ -96,18 +96,18 @@ int main(int argc, char** argv)
 
      pid_t originalPID = getpid();
 
-     //First part of the merging sort
+     // First part of the merging sort
      std::ifstream readedFile(input.c_str());
      callFork(min, max);
 
-     //Now only close buffer and all if I'm the original one
+     // Now only close buffer and all if I'm the original one
      if(originalPID == getpid()) {
           readedFile.close();
 
           ofile.close();
           ifile.close();
 
-          //So now, file is now sorted and writted in _sorted.bin
+          // So now, file is now sorted and writted in _sorted.bin
           createAnsiFile(output);
      }
      return 0;
@@ -117,7 +117,7 @@ int callFork(unsigned int min, unsigned int max)
 {
      pid_t pid_courant = getpid();
      pid_t pid_pere;
-     printf("Je suis le processus %d\n", pid_courant);
+     printf("I'm the first process number %d\n", pid_courant);
 
      int nbFils = 0;
      unsigned int minFils, maxFils;
@@ -126,7 +126,7 @@ int callFork(unsigned int min, unsigned int max)
           for (int i = 1; i <= 2; i++) {
                pid_pere = pid_courant;
 
-               // Determination des intervalles pour chaque fils
+               // Determining intervals for each son
                if (i == 1) {
                     minFils = min;
                     maxFils = (min + max) / 2;
@@ -135,29 +135,29 @@ int callFork(unsigned int min, unsigned int max)
                     maxFils = max;
                }
 
-               // Creation des fils
+               // Son's creation
                switch(fork()) {
-               case 0: // Fils
-                    // Mise a jour des valeurs
+               case 0: // Son's code
+                    // Value's update
                     pid_courant = getpid();
                     min = minFils;
                     max = maxFils;
                     printf("%d Created %d (%d/2); Interval : %d - %d\n", pid_pere, pid_courant, i, min, max);
                     break;
-               default: // Pere
+               default: // Father's code
                     nbFils++;
                     break;
                }
-               if (getpid() != pid_pere) // Si je suis le 1er fils
+               if (getpid() != pid_pere) // If I'm the first son
                     break;
           }
      }
 
-     //If i'm the father
+     // If I'm the father
      if (pid_pere == pid_courant) {
           printf("%d : Waiting\n", pid_courant);
 
-          //Wait until son finished
+          // Wait until son finished
           pid_t process;
           for (int j = 1; j <= 2; j++) {
                process = wait(NULL);
